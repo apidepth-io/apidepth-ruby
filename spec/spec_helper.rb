24c595b9269d23668687208c0f82a78d7e59957c
Chris Wright <cmwright33@gmail.com>
2026-05-24 16:39:04 -0400

    Add SimpleCov for coverage workflow

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 # spec/spec_helper.rb
 
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start do
+    add_filter "/spec/"
+    add_filter "/vendor/"
+  end
+end
+
 require "fileutils"
 require "webmock/rspec"
 require "apidepth"
